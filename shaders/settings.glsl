a370a6687fb2d27827f9b0cf40413cc731228989
asdf-hash <155254522+asdf-hash@users.noreply.github.com>
2026-02-19 19:11:36 -0500

    Add files via upload

---
+++ shaders/settings.glsl
@@ -0,0 +1,17 @@
+#define SHARPENING 2 // Sharpening filter. [0 1 2]
+#define CAS_AMOUNT 0.3 // Sharpening amount for CAS. [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0]
+
+// Distant Horizons fog start distance (0.0 = starts immediately, 1.0 = starts at max distance)
+#define DH_FOG_START 0.0 // [0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0]
+
+#define SSAO_SAMPLES 6 // [0 1 2 3 4 6 8 10 12]
+
+#define DH_OVERDRAW 0.0 // [0.0 0.6 0.7 0.8 0.9 0.95 0.97 0.99]
+
+#define DH_NOISE 1 // [1 0]
+
+#define SSAO_RADIUS 1.0 // [0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4]
+
+#define SSAO_INTENSITY 1.0 //[0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4]
+
+#define SSAO_BIAS 0.015 //[0.005 0.010 0.015 0.020 0.025 0.030]
